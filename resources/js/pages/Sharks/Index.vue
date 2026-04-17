1851506e7937dcd020fb80a258e0ee5d5d79fc8d
Kennu156 <kenmarttipaju@gmail.com>
2026-04-17 10:54:39 +0300

    hai api

--- resources/js/pages/Sharks/Index.vue
+++ resources/js/pages/Sharks/Index.vue
@@ -18,9 +18,9 @@ interface Shark {
     user?: { id: number; name: string };
 }
 
-const API_URL  = 'https://hajusrakendused-main-oewv1i.free.laravel.cloud/api/v1/films';
-const API_KEY  = 'IPTtKrAUan0K0gbfjbOZH4gQINTZZgDciVVdWgCvPsyuPb4r8oPDsiOcW6aG9Gkb';
-const IMG_BASE = 'https://hajusrakendused-main-oewv1i.free.laravel.cloud';
+const API_URL  = 'https://hajusrakendused-main-pm8ido.laravel.cloud/api/sharks';
+const API_KEY  = 'VyoBhOoGuiueem1rhoHjFIZUCLi9eLbBh1BdfDN5';
+const IMG_BASE = 'https://hajusrakendused-main-pm8ido.laravel.cloud';
 
 const sharks   = ref<Shark[]>([]);
 const loading  = ref(true);
